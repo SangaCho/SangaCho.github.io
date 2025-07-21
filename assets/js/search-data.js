d5a1d809f62aef3e84875d7fbf2fdd9cd7c44021
SangaCho <162191150+SangaCho@users.noreply.github.com>
2025-07-21 17:05:23 +0000

    Deploying to gh-pages from @ SangaCho/SangaCho.github.io@f8c84df28c3c807a978de9b472690a7e14c89ea3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -75,16 +75,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-sanga-cho-s-personal-homepage-is-now-live-welcome-and-thank-you-for-visiting",
+          title: 'Sanga Cho’s personal homepage is now live! Welcome and thank you for visiting....',
           description: "",
           section: "News",},{id: "projects-exogenous-attention-and-crf-fmri",
           title: 'Exogenous attention and CRF [fMRI]',
