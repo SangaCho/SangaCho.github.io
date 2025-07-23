0547ee9941be093efd9cc4cbf1172a4516bbbf8e
SangaCho <162191150+SangaCho@users.noreply.github.com>
2025-07-23 17:04:41 +0000

    Deploying to gh-pages from @ SangaCho/SangaCho.github.io@355f8c2a68811b57d7c921689cbddc6728a71f2c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,9 +55,9 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-attentional-allocation-across-large-visual-field",
+          title: 'Attentional allocation across large visual field',
+          description: "Efficiently allocated attentional gain across large visual field",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
