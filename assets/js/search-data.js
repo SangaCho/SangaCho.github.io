97db958027627aedbe6eb2bfc86e537455f89aa0
SangaCho <162191150+SangaCho@users.noreply.github.com>
2025-07-24 00:34:20 +0000

    Deploying to gh-pages from @ SangaCho/SangaCho.github.io@7b0531e5af56cf863641cc3393d058b48bf11b43 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,6 +65,31 @@ ninja.data = [{
           description: "AI",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-3d-brain",
+          title: '3D Brain',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-new-sports",
+          title: 'New sports',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
+            },},{id: "projects-bass-guitar",
+          title: 'Bass guitar',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/7_project/";
+            },},{id: "projects-planting",
+          title: 'Planting',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/8_project/";
+            },},{id: "projects-fine-art",
+          title: 'Fine Art',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
